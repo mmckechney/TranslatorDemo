d190103a3de604dd46f84f0bc016562e81a45dd7
Michael McKechney <michael@mckechney.com>
2023-12-21 12:54:40 -0500

    updated to object request (vs json string) and single document translation

--- Models/DocumentResult.cs
+++ Models/DocumentResult.cs
@@ -1,22 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text.Json.Serialization;
 
 namespace translator_demo.Models
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Text.Json.Serialization;
-
     public class DocumentResult
-    {
-        [JsonPropertyName("value")]
-        public List<Value> Value { get; set; }
-    }
-
-    public class Value
     {
         [JsonPropertyName("id")]
         public string Id { get; set; }
